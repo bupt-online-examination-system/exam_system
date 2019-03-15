f55bef7927b723d575adef67194897f854c3fc3a
ymh073819 <245213994@qq.com>
2019-03-16 00:56:02 +0800

    初始化远程仓库(把多个app改成了一个app多个views.py)

---
+++ exam_system/tools_views.py
@@ -0,0 +1,57 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+import time
+import random
+# import pandas
+from graduation_project import settings
+from django.http import HttpResponse
+from django.core.mail import send_mail
+
+#@login_required
+#python的装饰器，相当于java中的注解     @login_required表示必须登录才能访问，否则跳转到登录页面，在settings.py中配置LGOIN_URL参数（即登陆url）
+def start_exam(request):
+    if request.method == 'GET':
+        exam_time =  10     #获取设置的考试时间，转成秒
+        now_time = time.time()
+        #当前时间戳    这里应该判断一下该学生的状态是考试中还是未考试，如果是考试中，从数据库获取end_time(之后会把考试答卷改成异步上传，每次改动都传)。如果是未考试才获取当前时间
+        end_time = now_time + exam_time#截止时间戳
+        return render(request, "exam_interface.html",{"end_time":end_time*1000})#js中的时间戳是毫秒，转换一下
+    elif request.method == 'POST':
+        #存到数据库     算分，显示在页面上
+        pass
+        return render(request, "exam_result.html")
+
+def get_psw(request):
+    #为了防止代考，给每个考生生成6位随机口令 与考号绑定
+    if request.method == 'GET':
+        return render(request, "psw_out.html")
+    elif request.method == "post":
+        path = request['path']#或者request.POST.get('path'):
+        chars = 'abcdefghijklmnopqrstuvwxyz123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        # 随机产生6个不同字符
+        random_chars = "".join(random.sample(chars, 6))
+
+
+        return render(request, "psw_out.html")
+
+
+
+
+
+
+def send_email(request):
+    if request.method == "GET":
+        return render(request, "send_mail.html")
+    elif request.method == "POST":
+        name = request.POST.get('name', '')
+        subject = request.POST.get('subject','无标题')
+        text_content = request.POST.get('message','无内容')#提交的时候前端验证一下，不允许发空邮件   未完成
+        from_email = settings.DEFAULT_FROM_EMAIL
+        to_email = ['245213994@qq.com']  # 可以是多个
+        send_mail(
+            subject=subject,
+            message=text_content,
+            from_email=from_email,
+            recipient_list=to_email
+        )
+        return HttpResponse('发送成功')#然后返回主页  未完成
---
+++ graduation_project/urls.py
@@ -0,0 +1,32 @@
+"""exam_system URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import include, url
+from exam_system import tools_views
+from . import settings
+from django.views.static import serve
+from django.conf.urls.static import static
+#使用其他模块的东西要先import      as tool_views是为了区分不同模块的views
+urlpatterns = [
+    url(r'^start_exam/$', tools_views.start_exam, name='start_exam'),
+    url(r'^get_psw/$', tools_views.get_psw, name='get_psw'),
+    # url(r'^error/$', tools_views.error),
+    #name=xxx是为了在html中直接引用，例如href="/xxx/"   替代了href="127.0.0.1/xxx/"
+    url(r'^file/(?P<path>.*)$', serve, {'document_root':settings.MEDIA_ROOT}),
+    url(r'^send_email/$',tools_views.send_email,name='send_email'),
+]
+              #+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  这种方法仅仅在debug模式下起作用
