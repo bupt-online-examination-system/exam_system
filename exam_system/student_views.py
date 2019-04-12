eac270127082753ad21c8e48341d52516b9bafa3
zyc <2624501850@qq.com>
2019-04-12 22:29:51 +0800

    修复了一点小bug

--- exam_system/student_views.py
+++ exam_system/student_views.py
@@ -326,20 +326,21 @@ def pass_exam_details(request):    #已通过课程考试信息详情界面
     studentId=request.session.get('studentId')
     studentName = Person.objects.filter(userId=studentId).values('userName')
     courseId = request.GET.get("courseId")
-    exam_details_info = Exam.objects.filter(courseId=courseId).values('type','score')
+    exam_details_info = Exam.objects.filter(~Q(type=5),courseId=courseId).values('type','score')
     return render(request, "pass_exam_details.html", locals())
 
 def fail_exam_details(request):    #未通过课程考试信息详情界面
     studentId=request.session.get('studentId')
     studentName = Person.objects.filter(userId=studentId).values('userName')
     courseId = request.GET.get("courseId")
-    exam_details_info = Exam.objects.filter(courseId=courseId).values('type','score')
+    exam_details_info = Exam.objects.filter(~Q(type=5),courseId=courseId).values('type','score')
     return render(request, "fail_exam_details.html", locals())
 
 def exam_schedule(request):    #待考课程界面
     studentId=request.session.get('studentId')
     studentName = Person.objects.filter(userId=studentId).values('userName')
     exam_schedule_info = Course.objects.filter(isOver=1).values('courseId','courseName') #找出期末考未结束的课
+    print(exam_schedule_info)
     return render(request, "exam_schedule.html", locals())
 
 def exam_schedule_details(request):    #待考课程考试信息详情界面
