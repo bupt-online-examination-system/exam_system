92de977de43e115c881d3e1a9ea9875572703b19
guorui1028 <544677471@qq.com>
2019-04-22 17:35:08 +0800

    增添了部分页面及逻辑关系

--- exam_system/teacher_views.py
+++ exam_system/teacher_views.py
@@ -6,6 +6,25 @@ from django.contrib.auth.decorators import login_required
 def teacher_login(request): #教师界面模板
     return render(request, "teacher_login.html")
 
+def course_name(request):    #所授课程界面
+    teacherId = request.session.get('teacher_Id')
+    return render(request, "course_name.html")
+
+def first_exam(request):    #第一次月考
+    return render(request, "first_exam.html")
+
+def second_exam(request):    #第二次月考
+    return render(request, "second_exam.html")
+
+def third_exam(request):    #第三次月考
+    return render(request, "third_exam.html")
+
+def final_exam(request):    #最后一次考试
+    return render(request, "final_exam.html")
+
+def exam_number(request):    #选择第几次考试
+    return render(request, "exam_number.html")
+
 def student_name(request):    #学生名单界面
     return render(request, "student_name.html")
 
